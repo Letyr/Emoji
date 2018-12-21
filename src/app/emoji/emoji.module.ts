201e3327c0ab38900a03a034de4dad2336e6f3a3
Letyr <gofablov@gmail.com>
2018-12-22 01:50:52 +0800

    Чуть красивее с отвратительными костылями. Ибо я плохо верстаю

--- src/app/emoji/emoji.module.ts
+++ src/app/emoji/emoji.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { EmojiComponent } from './emoji/emoji.component';
+import {LightboxModule} from 'ngx-lightbox';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatButtonModule} from '@angular/material';
-import { LightboxModule } from 'ngx-lightbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -19,7 +23,11 @@ import { LightboxModule } from 'ngx-lightbox';
     BrowserAnimationsModule,
     MatButtonToggleModule,
     MatRadioModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatCardModule
   ],
   exports: [
     MainComponent,
